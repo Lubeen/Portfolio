b44ca322d86ce6c4039d65380e1c7224f1627e85
Luan Bezerra <luan.silvapvh@gmail.com>
2022-03-25 09:13:26 -0400

    estrutura do portfolio criada

---
+++ Estudos_code_show/exemplo1.py
@@ -0,0 +1,5 @@
+# http quer dizer protocolo de transferencia de hiper texto é um protocolo utilizado na transferencia entre cliente e servidor
+#
+# Voce vai montar o significado semantico da sua pagina em html
+
+# Verbos http
