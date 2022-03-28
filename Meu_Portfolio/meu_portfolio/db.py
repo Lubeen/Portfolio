82430c5f56bb1a5038c1f65dcdafc055a708b7e2
Luan Bezerra <luan.silvapvh@gmail.com>
2022-03-28 11:11:29 -0400

    dict como banco de dados para brincar

---
+++ Meu_Portfolio/meu_portfolio/db.py
@@ -0,0 +1,14 @@
+users = {
+    'jose': {
+        'name': 'Jose da silva montenegro',
+        'age': '25',
+        'location': 'Sao Paulo',
+        'telephone': '+55 11 99999-9999',
+},
+    'maria': {
+        'name': 'Maria da silva montenegro',
+        'age': '25',
+        'location': 'Sao Paulo',
+        'telephone': '+55 11 99999-9999',
+    }
+}
